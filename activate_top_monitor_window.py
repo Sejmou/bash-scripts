314559d57be93e79ab517a0f4662fade667ef386
Sejmou <samo.kolter@gmail.com>
2022-03-31 15:49:33 +0200

    FINALLY fix Python script for activating top window of selected monitor

--- activate_top_monitor_window.py
+++ activate_top_monitor_window.py
@@ -1,9 +1,8 @@
-import enum
 import subprocess
 import re
-import math
 import sys
 from collections import defaultdict
+import warnings
 
 
 def main():
@@ -12,8 +11,6 @@ def main():
         raise ValueError("Single integer argument (selected monitor) expected!")
 
     selected_monitor = int(sys.argv[1])
-
-    desktop_width = get_desktop_width()
     monitor_x_offsets = get_monitor_x_offsets()
     no_of_monitors = len(monitor_x_offsets)
 
@@ -23,13 +20,12 @@ def main():
         )
 
     window_ids_by_z_idx = get_window_ids_by_z_idx()
+    window_x_positions = get_window_x_positions(window_ids_by_z_idx)
+    window_ids_and_positions = zip(window_ids_by_z_idx, window_x_positions)
 
-    window_monitors = get_window_monitors(desktop_width, monitor_x_offsets)
-
-    windows_per_monitor = defaultdict(list)
-    for window_id in window_ids_by_z_idx:
-        monitor = window_monitors[window_id]
-        windows_per_monitor[monitor].append(window_id)
+    windows_per_monitor = get_windows_per_monitor(
+        window_ids_and_positions, monitor_x_offsets
+    )
 
     subprocess.run(
         f"wmctrl -i -a {windows_per_monitor[selected_monitor][-1]}".split(" ")
@@ -40,54 +36,24 @@ def consecutive_whitespace_except_newline_to_space(str):
     return re.sub("[^\S\n]+", " ", str)
 
 
-def get_desktop_width():
-    return int(
-        # select currently active desktop (marked with * in output of wmctrl -d)
-        # then search for field containing desktop dimensions (widthxheight) and extract width
-        subprocess.getoutput("wmctrl -d | grep \* | cut -d' ' -f5 | cut -d'x' -f1")
-    )
-
-
-def get_window_monitors(desktop_width, x_offsets):
-    window_info_lines = consecutive_whitespace_except_newline_to_space(
-        subprocess.getoutput("wmctrl -lG")
-    ).split("\n")
-
-    print(x_offsets)
-
+def get_windows_per_monitor(window_ids_and_positions, x_offsets):
     def get_monitor(x_pos):
-        while x_pos > desktop_width:
-            x_pos = x_pos - desktop_width + 1
-
+        # could be written in a smarter way, I know...
         monitor = 0
         x_offset = 0
-        print(x_pos)
 
         for curr_monitor, curr_offset in enumerate(x_offsets):
-            print(
-                f"{curr_monitor}:",
-                curr_offset,
-                curr_offset + 1920 if curr_monitor < len(x_offsets) - 1 else "(last)",
-            )
             if curr_offset <= x_pos and curr_offset >= x_offset:
                 monitor = curr_monitor
                 x_offset = curr_offset
 
-        print("selected:", monitor)
         return monitor
 
-    def get_window_id_and_monitor(window_info_line):
-        line_cols = window_info_line.split(" ")
-        window_id = int(line_cols[0], 16)
-        window_xpos = int(line_cols[2])
+    windows_per_monitor = defaultdict(list)
+    for id, pos in window_ids_and_positions:
+        windows_per_monitor[get_monitor(pos)].append(id)
 
-        print()
-        print(hex(window_id))
-        print(window_xpos)
-
-        return window_id, get_monitor(window_xpos)
-
-    return dict(get_window_id_and_monitor(line) for line in window_info_lines)
+    return windows_per_monitor
 
 
 def get_window_ids_by_z_idx():
@@ -101,21 +67,44 @@ def get_window_ids_by_z_idx():
     return [int(id_str, 16) for id_str in window_id_strings]
 
 
+def get_window_x_position(window_id):
+    # if you're wondering: can't use wmctrl -lG to get window x positions as its output is not reliable, some x-coordinates are just BS lol
+    # probably the issue is the arrangment of monitors in a multi-monitor setup (for certain types of windows it is respected, for others not...)
+    cmd = f"xwininfo -id {hex(window_id)} | grep 'Absolute upper-left X'"
+    xwininfo_out = subprocess.getoutput(cmd)
+    search_result = re.search(r"[0-9]+", xwininfo_out)
+    if search_result != None:
+        x_pos = int(search_result.group(0))
+        return x_pos
+    else:
+        warnings.warn(
+            f"output of xwinfo command ({cmd}) was:",
+            xwininfo_out,
+            "\nCould not find x-coordinate of window",
+        )
+
+
+def get_window_x_positions(window_ids):
+    x_positions = [get_window_x_position(id) for id in window_ids]
+    return x_positions
+
+
 def get_monitor_x_offsets():
     xrandr_out = subprocess.getoutput("xrandr | grep '\sconnected'").split("\n")
 
     def extract_monitor_x_offset(xrandr_out_line):
-        # width = re.search(r"[0-9]+x[0-9]+", xrandr_out_line).group(0).split("x")[0]
         match = re.search(r"\+[0-9]+", xrandr_out_line)
         if match:
-            xoffset = int(match.group(0)[1:])
-            return xoffset
+            x_offset = int(match.group(0))
+            return x_offset
 
     x_offsets = []
     for line in xrandr_out:
         x_offset = extract_monitor_x_offset(line)
         if x_offset != None:  # monitor connected
             x_offsets.append(x_offset)
+
+    x_offsets.sort()
 
     return x_offsets
 
