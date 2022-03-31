c9b035ef3b646484a97cebdec12be304502dc311
Sejmou <samo.kolter@gmail.com>
2022-03-31 14:38:42 +0200

    trying to fix Python script for activating top window of selected monitor - realized output of wmctrl -lG is just garbage yaaaaay

--- activate_top_monitor_window.py
+++ activate_top_monitor_window.py
@@ -1,3 +1,4 @@
+import enum
 import subprocess
 import re
 import math
@@ -12,20 +13,21 @@ def main():
 
     selected_monitor = int(sys.argv[1])
 
-    monitor_width = 1920
     desktop_width = get_desktop_width()
-    no_of_monitors = int(desktop_width / monitor_width)
+    monitor_x_offsets = get_monitor_x_offsets()
+    no_of_monitors = len(monitor_x_offsets)
 
     if selected_monitor < 0 or selected_monitor >= no_of_monitors:
         raise ValueError(
             f"Invalid monitor ID! Choose either from {list(range(no_of_monitors))}, as {no_of_monitors} monitors are connected."
         )
 
-    windows_by_z_idx = get_windows_by_z_idx()
-    window_monitors = get_window_monitors(desktop_width, monitor_width)
+    window_ids_by_z_idx = get_window_ids_by_z_idx()
+
+    window_monitors = get_window_monitors(desktop_width, monitor_x_offsets)
 
     windows_per_monitor = defaultdict(list)
-    for window_id in windows_by_z_idx:
+    for window_id in window_ids_by_z_idx:
         monitor = window_monitors[window_id]
         windows_per_monitor[monitor].append(window_id)
 
@@ -46,24 +48,49 @@ def get_desktop_width():
     )
 
 
-def get_window_monitors(desktop_width, monitor_width):
+def get_window_monitors(desktop_width, x_offsets):
     window_info_lines = consecutive_whitespace_except_newline_to_space(
         subprocess.getoutput("wmctrl -lG")
     ).split("\n")
 
+    print(x_offsets)
+
     def get_monitor(x_pos):
-        return math.floor(x_pos / monitor_width)
+        while x_pos > desktop_width:
+            x_pos = x_pos - desktop_width + 1
+
+        monitor = 0
+        x_offset = 0
+        print(x_pos)
+
+        for curr_monitor, curr_offset in enumerate(x_offsets):
+            print(
+                f"{curr_monitor}:",
+                curr_offset,
+                curr_offset + 1920 if curr_monitor < len(x_offsets) - 1 else "(last)",
+            )
+            if curr_offset <= x_pos and curr_offset >= x_offset:
+                monitor = curr_monitor
+                x_offset = curr_offset
+
+        print("selected:", monitor)
+        return monitor
 
     def get_window_id_and_monitor(window_info_line):
         line_cols = window_info_line.split(" ")
         window_id = int(line_cols[0], 16)
-        window_xpos = int(line_cols[2], 10)
+        window_xpos = int(line_cols[2])
+
+        print()
+        print(hex(window_id))
+        print(window_xpos)
+
         return window_id, get_monitor(window_xpos)
 
     return dict(get_window_id_and_monitor(line) for line in window_info_lines)
 
 
-def get_windows_by_z_idx():
+def get_window_ids_by_z_idx():
     """
     returns all windows of the desktop, ordered by z-index (caution: index 0 is furthest away from top!)
     """
@@ -72,6 +99,25 @@ def get_windows_by_z_idx():
     no_whitespace = "".join(xprop_output.split())
     window_id_strings = no_whitespace[no_whitespace.index("#") + 1 :].split(",")
     return [int(id_str, 16) for id_str in window_id_strings]
+
+
+def get_monitor_x_offsets():
+    xrandr_out = subprocess.getoutput("xrandr | grep '\sconnected'").split("\n")
+
+    def extract_monitor_x_offset(xrandr_out_line):
+        # width = re.search(r"[0-9]+x[0-9]+", xrandr_out_line).group(0).split("x")[0]
+        match = re.search(r"\+[0-9]+", xrandr_out_line)
+        if match:
+            xoffset = int(match.group(0)[1:])
+            return xoffset
+
+    x_offsets = []
+    for line in xrandr_out:
+        x_offset = extract_monitor_x_offset(line)
+        if x_offset != None:  # monitor connected
+            x_offsets.append(x_offset)
+
+    return x_offsets
 
 
 main()
