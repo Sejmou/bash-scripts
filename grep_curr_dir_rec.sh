c371802f27ad148b6ee55049a1cb7dbe7fa362d5
Sejmou <samo.kolter@gmail.com>
2022-06-08 13:08:57 +0200

    add utility script for text recursive grep through files of current folder

---
+++ grep_curr_dir_rec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this program does a recursive search through non-binary text files of the current folder (incl. subfolders), ignoring hidden files
+# pro tip: add file manager context menu entry with filemanager-actions (launch gnome-terminal -- path/to/this/script)
+echo "Text (regex) search through text files in this folder + subfolders"
+echo "Type the pattern you wish to search for:"
+read pattern
+# recursive search from current directory, ignoring binary files and excluding hidden directories (starting with ".")
+grep -rI --exclude-dir='.*' ${pattern} .
+read -p "Done searching, press Enter to close this dialog"
