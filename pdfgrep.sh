a54e961a18b846fe24b6887466ec53ed7aa32305
Sejmou <samo.kolter@gmail.com>
2022-06-08 12:54:15 +0200

    pdfgrep.sh: fix issue with spaces in path

--- pdfgrep.sh
+++ pdfgrep.sh
@@ -4,5 +4,6 @@
 echo "Text (regex) search through PDF files in this folder + subfolders"
 echo "Type the pattern you wish to search for:"
 read pattern
-pdfgrep ${pattern} $(find -name '*.pdf' | sort)
+# find paths to all pdf files, sort alphabetically, wrap with quotes to make sure space in paths are not an issue, call pdfgrep with pattern on each path
+find -name '*.pdf' | sort | sed 's/^/"/;s/$/"/' | xargs pdfgrep ${pattern}
 read -p "Done searching, press Enter to close this dialog"
