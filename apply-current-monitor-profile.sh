9fdc1ddc1a050ec6b048045b85519849e5fb6e76
Sejmou <samo.kolter@gmail.com>
2022-06-14 14:47:29 +0200

    remove hardcoded .sh extension in monitor profile scripts

--- apply-current-monitor-profile.sh
+++ apply-current-monitor-profile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# this is part of a workaround to make sure monitor profiles are automatically loaded in Ubuntu on login
-# add this file to startup applications (with ca. 5 seconds sleep added beforehand to increase probability that desktop is fully loaded and profile can be applied)
-# use in tandem with apply-and-persist-monitor-profile.sh
-# assumes that xrandr scripts are stored in $HOME/.screenlayout
-# loads the script currently stored in the current-profile file in the same folder
-current=`cat $HOME/.screenlayout/current-profile`.sh
-$HOME/.screenlayout/$current
+# this is part of a workaround to make sure correct monitor profiles are automatically loaded in Ubuntu on login
+# The script assumes that xrandr scripts for applying monitor profiles are all stored in $HOME/.screenlayout
+# it loads the script whose name is stored in the "current-profile" file in the same folder
+# use in tandem with apply-and-persist-monitor-profile
+# add this file to $HOME/config/autostart folder to make sure it runs right after login (when X11 and gdm3 are ready)
+# please also let me know in case you found a solution to run the script successfully BEFORE the login screen is shown (googled for a few hours, without luck)
+# with the current setup, the monitor configuration is still wrong on the login screen :/
+xrandr_folder=$HOME/.screenlayout
+current=`cat $xrandr_folder/current-profile`
+$xrandr_folder/$current
