e3d065adfe2507d7570c877d008e8b03088f1a7c
Sejmou <samo.kolter@gmail.com>
2022-06-14 15:07:27 +0200

    update readme of apply-current-monitor-profile

--- apply-current-monitor-profile.sh
+++ apply-current-monitor-profile.sh
@@ -3,7 +3,14 @@
 # The script assumes that xrandr scripts for applying monitor profiles are all stored in $HOME/.screenlayout
 # it loads the script whose name is stored in the "current-profile" file in the same folder
 # use in tandem with apply-and-persist-monitor-profile
-# add this file to $HOME/config/autostart folder to make sure it runs right after login (when X11 and gdm3 are ready)
+
+# To make sure the script runs right after login (when X11 and gdm3 are ready) add a .desktop file (e.g. monitorprofile.desktop) to $HOME/config/autostart folder
+# it should have the following content (without hashes/comments!):
+#[Desktop Entry]
+#Type=Application
+#Name=monitorprofile
+#Exec=/home/sejmou/Repos/Bash/bash-scripts/apply-current-monitor-profile.sh
+
 # please also let me know in case you found a solution to run the script successfully BEFORE the login screen is shown (googled for a few hours, without luck)
 # with the current setup, the monitor configuration is still wrong on the login screen :/
 xrandr_folder=$HOME/.screenlayout
